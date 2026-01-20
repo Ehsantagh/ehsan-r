52a7e99f040db9d83200a523a238698d4a062ed2
Ehsantagh <ehsan.taghaddosi@gmail.com>
2026-01-20 08:02:24 +0000

    footer is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 // src/App.jsx
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Menu from './components/Menu';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 import Expertise from './pages/Expertise';
 import WorkList from './pages/WorkList';
@@ -11,10 +12,12 @@ import About from './pages/About';
 import CV from './pages/CV';
 import Contact from './pages/Contact';
 
-function App() {
+function AppContent() {
+  const location = useLocation();
+  const isWhiteFooter = location.pathname === '/torch' || location.pathname === '/contact';
+
   return (
-    <Router>
-      <Menu />
+    <>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
@@ -26,6 +29,16 @@ function App() {
         <Route path="/CV" element={<CV />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer isWhite={isWhiteFooter} />
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <Menu />
+      <AppContent />
     </Router>
   );
 }
